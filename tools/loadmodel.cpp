d0a4b2d8d12679d73d22370c830f1b3d2c49e6d0
Steven Atkinson <steven@atkinson.mn>
2023-10-01 12:53:45 -0700

    Reworking testing files

--- tools/loadmodel.cpp
+++ tools/loadmodel.cpp
@@ -5,35 +5,62 @@
  */
 
 // Test loading a model
+// FIXME let's get some proper testing going!
 
 #include <stdlib.h>
-#include "NAM/dsp.h"
+#include "dsp/dsp.h"
+
+class Dummy: public dsp::DSP {
+  public:
+  ~Dummy() {
+    _DeallocateOutputPointers();
+  }
+  DSP_SAMPLE** Process(DSP_SAMPLE** inputs, const size_t numChannels, const size_t numFrames) override {
+    if (numChannels > _GetNumChannels()) {
+      throw std::runtime_error("Asked to process too many channels!\n");
+    }
+    if (numFrames > _GetNumFrames()) {
+      throw std::runtime_error("Asked to process too many samples!\n");
+    }
+    for (int c = 0; c < numChannels; c++) {
+      for (int f=0; f<numFrames; f++) {
+        mOutputs[c][f] = inputs[c][f];
+      }
+    }
+    return _GetPointers();
+  };
+  void PrepareForAudio(const int maxChannels, const int maxFrames) {
+    _PrepareBuffers(maxChannels, maxFrames);
+  };
+};
 
 int main(int argc, char* argv[])
 {
-  if (argc > 1)
-  {
-    char* modelPath = argv[1];
-
-    fprintf(stderr, "Loading model [%s]\n", modelPath);
-
-    auto model = get_dsp(modelPath);
-
-    if (model != nullptr)
-    {
-      fprintf(stderr, "Model loaded successfully\n");
-    }
-    else
-    {
-      fprintf(stderr, "Failed to load model\n");
-
-      exit(1);
-    }
+  Dummy dummy;
+  const int maxChannels = 2;
+  const int maxSamples = 128;
+  dummy.PrepareForAudio(maxChannels, maxSamples);
+  DSP_SAMPLE** inputs = new DSP_SAMPLE*[maxChannels];
+  for (int c = 0; c<maxChannels; c++) {
+    inputs[c] = new DSP_SAMPLE[maxSamples];
   }
-  else
-  {
-    fprintf(stderr, "Usage: loadmodel <model_path>\n");
+    
+  const int numBuffers = 2;
+  for (int b = 0; b< numBuffers; b++) {
+    // Fill the input buffer
+    for (int c = 0; c < maxChannels; c++) {
+      for (int s=0; s<maxSamples; s++) {
+        inputs[c][s] = 0.01 * c + 0.02 * s / (DSP_SAMPLE) maxSamples;
+      }
+    }
+    dummy.Process(inputs, maxChannels, maxSamples);
+    // Yay
   }
 
-  exit(0);
+  // Deallocate
+  for (int c = 0; c<maxChannels; c++)
+    delete [] inputs[c];
+  delete [] inputs;
+
+  return 0;
 }
